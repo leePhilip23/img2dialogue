84e234d52b9cda35f05d41dfbf9b7eb1d625c70a
Philip Lee <leephiliphl@gmail.com>
2025-07-02 23:55:14 -0500

    Edit dataloader

--- src/utils/loader.py
+++ src/utils/loader.py
@@ -1,31 +1,18 @@
-import torch
+import random
 from torch import Tensor
-from typing import Any
 from PIL import Image
 from torch.utils.data import Dataset
 from datasets import load_dataset, DatasetDict
 from transformers import (
-    AutoTokenizer,
     BlipImageProcessorFast,
     CLIPImageProcessorFast,
 )
+from .exceptions import (
+    HuggingFaceDataNotFound,
+    ImgProcessorNotSupported,
+    ImgProcessorNotFound
+)
 from utils import log
-from __future__ import annotations
-
-
-class HuggingFaceDataNotFound(Exception):
-    """Hugging Face Dataset not found"""
-    pass
-
-
-class ImgProcessorNotFound(Exception):
-    """Hugging Face Image Processor not found"""
-    pass
-
-
-class TxtTokenizerNotFound(Exception):
-    """Hugging Face Text Tokenizer not found"""
-    pass
 
 
 class CustomImageDataset(Dataset):
@@ -33,15 +20,13 @@ class CustomImageDataset(Dataset):
         self,
         split: str,
         data_name: str = "HuggingFaceM4/VisDial",
-        img_process: str = "Salesforce/blip-image-captioning-base",
-        txt_process: str = "Qwen/Qwen3-0.6B",
+        img_process: str = "Salesforce/blip-image-captioning-base"
     ):
         self._data = self._load_data_hf(data_name, split)
         self.img_processor = self._load_img_process(img_process)
-        self.txt_tokenizer = self._load_txt_token(txt_process)
 
 
-    def _load_data_hf(self, url: str, split: str) -> CustomImageDataset:
+    def _load_data_hf(self, url: str, split: str) -> DatasetDict:
         """
         Loads the data and returns it
 
@@ -49,7 +34,7 @@ class CustomImageDataset(Dataset):
             url (str): Directory of Hugging Face dataset
         
         Returns: 
-            CustomImageDataset: Dataset downloaded from Hugging Face
+            DatasetDict: Dataset downloaded from Hugging Face
 
         Raises:
             HFDataNotFound
@@ -60,7 +45,8 @@ class CustomImageDataset(Dataset):
                 ["caption", "image_path", "global_image_id", "anns_id"]
             )
         except HuggingFaceDataNotFound:
-            log.exception(f"Dataset {url} not found")
+            log.error(f"Dataset {url} not found")
+            raise
         return data[split]
 
 
@@ -78,39 +64,16 @@ class CustomImageDataset(Dataset):
             ImgProcessorNotFound
         """
         try:
-            if "blip" in img_model_name.lower():
-                processor = BlipImageProcessorFast.from_pretrained(img_model_name)
-            elif "clip" in img_model_name.lower():
-                processor = CLIPImageProcessorFast.from_pretrained(img_model_name)
+            if 'blip' in img_model_name.lower():
+                return BlipImageProcessorFast.from_pretrained(img_model_name)
+            elif 'clip' in img_model_name.lower():
+                return CLIPImageProcessorFast.from_pretrained(img_model_name)
+            else:
+                log.error(f"Processor: {img_model_name} is not a valid choice")
+                raise ImgProcessorNotSupported("Processor type is not among the supported choices")
         except ImgProcessorNotFound:
-            log.exception(
-                f"Processor not found for: {img_model_name} in Hugging Face Directory"
-            )
-        return processor
-
-
-    def _load_txt_token(self, txt_token_name: str) -> AutoTokenizer:
-        """
-        Loads the text tokenizer and returns it
-
-        Args:
-            url (str): Directory of Hugging Face Tokenizer
-        
-        Returns: 
-            AutoTokenizer: Tokenizer downloaded from Hugging Face
-
-        Raises:
-            TxtTokenizerNotFound
-        """
-        try:
-            tokenizer = AutoTokenizer.from_pretrained(
-                txt_token_name, padding_side="left", bos_token="<BOS>", eos_token="<EOS>"
-            )
-        except TxtTokenizerNotFound:
-            log.exception(
-                f"Tokenizer not found for: {txt_token_name} in Hugging Face Directory"
-            )
-        return tokenizer
+            log.error(f"Image Processor: {img_model_name} is not found in HF repository")
+            raise
 
 
     def _process_img(self, img: Image) -> Tensor:
@@ -130,22 +93,21 @@ class CustomImageDataset(Dataset):
             tuple[Tensor, Tensor]: Preprocessed text and attention masks to
                                    prevent gradient calculations for padding
         """
-        dialogue = " ".join(f"[Q] {q} [/Q] [A] {a} [/A]" for q, a in txt)
-        dialogue = f"<BOS> {dialogue} <EOS>"
-        inputs_masks = self.txt_tokenizer(
-            dialogue, padding=True, return_tensors="pt"
-        )
-        return inputs_masks.input_ids[0], inputs_masks.attention_mask[0]
-
+        r_index = random.randint(0, len(txt)-1)
+        return f"[Q] {txt[r_index][0]} [/Q] {txt[r_index][1]} <EOS>"
+    
 
     def __len__(self) -> int:
         """Returns the length of batched dataset"""
         return len(self._data)
 
 
-    def __getitem__(self, idx: int) -> tuple[Tensor, Tensor, Tensor]:
+    def __getitem__(self, idx: int) -> dict[str: Tensor]:
         """Preprocesses each datapoint and returns image, text, and masking tensors"""
         txt, img = self._data[idx]
         img_input = self._process_img(img)
-        txt_input, txt_mask = self._process_txt(txt)
-        return img_input, txt_input, txt_mask
+        txt_input =  self._process_txt(txt)
+        return {
+            "img": img_input, 
+            "txt": txt_input
+        }
