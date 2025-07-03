d130c2f1674d02f1d9a452ed0a60a91ba06c46f0
Philip Lee <leephiliphl@gmail.com>
2025-07-02 23:54:41 -0500

    Fix loggers configurations

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,8 @@
 import logging 
 
 log = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.DEBUG, 
+    format='[%(asctime)s] [%(levelname)s]: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
