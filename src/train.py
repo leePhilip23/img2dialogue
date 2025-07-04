2746d2fdd4bd396431fbf5b7f2d03b903deeb08f
Philip Lee <leephiliphl@gmail.com>
2025-07-03 19:22:05 -0500

    Fetch data to train

--- src/train.py
+++ src/train.py
@@ -1,13 +1,32 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from torch.utils.data import DataLoader
+from .utils.loader import CustomDataset
 from src import log
 
-def train():
-    
+
+def get_data() -> tuple[DataLoader, DataLoader]:
+    train_data = CustomDataset(
+        "train", 
+        "HuggingFaceM4/VisDial", 
+        "Salesforce/blip-image-captioning-base"
+    )
+    valid_data = CustomDataset(
+        "valid", 
+        "HuggingFaceM4/VisDial", 
+        "Salesforce/blip-image-captioning-base"
+    )
+
+    #TODO: Put config in hydra
+    train_dataloader = DataLoader(train_data, batch_size=64, num_workers=4, pin_memory=True, shuffle=True)
+    valid_dataloader = DataLoader(valid_data, batch_size=64, num_workers=4, pin_memory=True, shuffle=False)
+
+    return train_dataloader, valid_dataloader
+
 
 def main():
+    train_loader, valid_loader = get_data()
     
     
 if __name__ == '__main__':
