caece46b03a826cf74a60786e333841f5e91557d
Philip Lee <leephiliphl@gmail.com>
2025-07-04 23:58:52 -0500

    Added important libraries and functions to aid in training

--- src/train.py
+++ src/train.py
@@ -1,33 +1,70 @@
+import os
+import hydra
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch import device
 from torch.utils.data import DataLoader
+from torch.optim import AdamW
 from .utils.loader import CustomDataset
+from .utils.model import Model
 from src import log
 
 
-def get_data() -> tuple[DataLoader, DataLoader]:
+def _get_device() -> device:
+    """Sets the device based on hardware available"""
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    return torch.device("cpu")
+
+
+def _get_model(cfg) -> Model:
+    """Initializes the model with the available hardware"""
+    device = _get_device()
+    return Model(
+        img_model=cfg.model.img_model,
+        small_lm=cfg.model.small_lm
+    ).to(device)
+    
+
+def _get_data(cfg) -> tuple[DataLoader, DataLoader]:
+    """Loads the training and validation data from the dataset"""
     train_data = CustomDataset(
-        "train", 
-        "HuggingFaceM4/VisDial", 
-        "Salesforce/blip-image-captioning-base"
+        cfg.config.train, 
+        cfg.config.path, 
+        cfg.model.img_model
     )
     valid_data = CustomDataset(
-        "valid", 
-        "HuggingFaceM4/VisDial", 
-        "Salesforce/blip-image-captioning-base"
+        cfg.config.valid, 
+        cfg.config.path, 
+        cfg.model.img_model
     )
 
-    #TODO: Put config in hydra
-    train_dataloader = DataLoader(train_data, batch_size=64, num_workers=4, pin_memory=True, shuffle=True)
-    valid_dataloader = DataLoader(valid_data, batch_size=64, num_workers=4, pin_memory=True, shuffle=False)
+    train_dataloader = DataLoader(
+        train_data, 
+        batch_size=cfg.train.batch_size, 
+        num_workers=cfg.train.num_workers, 
+        pin_memory=cfg.train.pin_memory, 
+        shuffle=cfg.train.shuffle
+    )
+    valid_dataloader = DataLoader(
+        valid_data, 
+        batch_size=cfg.valid.batch_size, 
+        num_workers=cfg.valid.num_workers, 
+        pin_memory=cfg.valid.pin_memory, 
+        shuffle=cfg.valid.shuffle
+    )
 
     return train_dataloader, valid_dataloader
 
 
-def main():
-    train_loader, valid_loader = get_data()
-    
+@hydra.main(config_path="conf", config_name="config.yml")
+def main(cfg):
+    """Main function to run the training process"""
+    train_loader, valid_loader = _get_data()
+
     
 if __name__ == '__main__':
     main()
