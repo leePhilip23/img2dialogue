3c832feadf28bddf970470ca906f685583135a9f
Philip Lee <leephiliphl@gmail.com>
2025-08-06 22:31:44 -0500

    Change preprocess due to dataset

--- src/utils/loader.py
+++ src/utils/loader.py
@@ -1,4 +1,3 @@
-import random
 from torch import Tensor
 from PIL.Image import Image
 from torch.utils.data import Dataset
@@ -7,46 +6,23 @@ from transformers import (
     BlipImageProcessorFast,
     CLIPImageProcessorFast,
 )
+from src import log
 from .exceptions import (
     HuggingFaceDataNotFound,
     ImgProcessorNotSupported,
     ImgProcessorNotFound
 )
-from utils import log
 
 
 class CustomDataset(Dataset):
     def __init__(
         self,
         split: str,
-        data_name: str = "HuggingFaceM4/VisDial",
+        data_name: str = "lmms-lab/LLaVA-NeXT-Data",
         img_process: str = "Salesforce/blip-image-captioning-base"
     ):
         self.img_processor = self._load_img_process(img_process)
         self._data = self._load_data_hf(data_name, split)
-
-
-    
-    def _preprocess_data(self, data: DatasetDict) -> list[tuple[tuple[str], Image]]:
-        """
-        Preprocesses the data by extracting questions, answers, and images.
-        Returns a list of tuples containing question-answer pairs and the image.
-
-        Args:
-            data (DatasetDict): Dataset downloaded from Hugging Face
-        Returns:
-            list[tuple[tupe[str], Image]]: List of tuples with question-answer pairs and image
-        """
-        processed_data = []
-        for i, item in enumerate(data):  
-            txt, label = self._process_txt(item["dialog"])
-            if not txt or not label:
-                continue
-
-            image = self._process_img(item["image"])   
-            processed_data.append((image, txt, label))
-            count += 1
-        return processed_data
 
 
     def _load_data_hf(self, url: str, split: str) -> DatasetDict:
@@ -61,15 +37,8 @@ class CustomDataset(Dataset):
             HFDataNotFound
         """
         try:
-            data = load_dataset(url, split=split, num_proc=1, keep_in_memory=False)
-            data = data.remove_columns(
-                [
-                    "caption", 
-                    "image_path", 
-                    "global_image_id", 
-                    "anns_id"
-                ]
-            )
+            data = load_dataset(url, split=split, num_proc=1, keep_in_memory=True, seed=None)
+            data = data.remove_columns(["id", "data_source"])
             preprocess_data = self._preprocess_data(data)
         except HuggingFaceDataNotFound:
             log.error(f"Dataset {url} not found")
@@ -93,8 +62,9 @@ class CustomDataset(Dataset):
                 return BlipImageProcessorFast.from_pretrained(img_model_name)
             elif 'clip' in img_model_name.lower():
                 return CLIPImageProcessorFast.from_pretrained(img_model_name)
-            log.error(f"Processor: {img_model_name} is not a valid choice")
-            raise ImgProcessorNotSupported("Processor type is not among the supported choices")
+            else:
+                log.error(f"Processor: {img_model_name} is not a valid choice")
+                raise ImgProcessorNotSupported("Processor type is not among the supported choices")
         except ImgProcessorNotFound:
             log.error(f"Image Processor: {img_model_name} is not found in HF repository")
             raise
@@ -105,7 +75,7 @@ class CustomDataset(Dataset):
         return self.img_processor(img).pixel_values[0]
 
 
-    def _process_txt(self, txt: list[list[str]]) -> tuple[Tensor, Tensor]:
+    def _process_txt(self, txt: dict[str, str]) -> tuple[str, str]:
         """
         Preprocesses by adding special tokens to each question and answer.
         Then it tokenizes the raw text.
@@ -113,24 +83,34 @@ class CustomDataset(Dataset):
         Args:
             txt (list[list[str]]): Each matrix row is a question and answer
         Returns:
-            tuple[Tensor, Tensor]: Preprocessed text and attention masks to
+            tuple[str, str]: Preprocessed text and attention masks to
                                    prevent gradient calculations for padding
         """
-        qa_pairs, last_ans = "", ""
-        for i, qa in enumerate(txt):
-            # Ensure that both question and answer are non-empty
-            if not qa[0] or not qa[1]:
-                continue
-            
-            #TODO: Add special case where last qa doesnt pass above condition
-            #TODO: Add test cases to validate data preprocessing
-            if i+1 >= len(txt):
-                qa_pairs += f"[Q] {qa[0]} [/Q] <EOS>"
+        qa_pairs = "<SOS> ", 
+        for i in range(len(txt)-1):
+            if txt[i]['from'] == "human":
+                qa_pairs += f"[Q] {txt[i]['value']} [/Q] "
             else:
-                qa_pairs += f"[Q] {qa[0]} [/Q] {qa[1]} "
-            last_ans = qa[1]
-            
-        return qa_pairs, last_ans
+                qa_pairs += f"{txt[i]['value']} "
+        return qa_pairs + "<EOS>", txt[-1]['value']
+    
+
+    def _preprocess_data(self, data: DatasetDict) -> list[tuple[Image, str, str]]:
+        """
+        Preprocesses the data by extracting questions, answers, and images.
+        Returns a list of tuples containing question-answer pairs and the image.
+
+        Args:
+            data (DatasetDict): Dataset downloaded from Hugging Face
+        Returns:
+            list[tuple[tupe[str], Image]]: List of tuples with question-answer pairs and image
+        """
+        processed_data = []
+        for item in data:
+            image = self._process_img(item["image"])  
+            txt, label = self._process_txt(item["conversations"]) 
+            processed_data.append((image, txt, label))
+        return processed_data
     
 
     def __len__(self) -> int:
