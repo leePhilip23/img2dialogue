0c77d1b22571d56963863c99ad9a4560cd589027
Philip Lee <leephiliphl@gmail.com>
2025-08-06 22:30:41 -0500

    Test if training is working

---
+++ testing/code/test_train.py
@@ -0,0 +1,21 @@
+from hydra import initialize, compose
+from src.utils.specs import Retrieve
+from src.utils.train import train_data, run_training
+
+
+def test_model() -> None:
+    device = Retrieve.get_device()
+    model = Retrieve.get_model(cfg, device)
+    with initialize(config_path="../../conf", version_base=None):
+        cfg = compose(config_name="config")
+        train_loader, valid_loader = train_data(cfg)
+        loss = run_training(
+            cfg, 
+            train_loader, 
+            valid_loader, 
+            model, 
+            device,
+            cfg.train_param.t_epochs,
+            testing=True
+        )
+        assert loss[0] > loss[1], "Loss does not decrease"     
