946a87424e7a7785e07a4c282abcf92906f0d8db
Philip Lee <leephiliphl@gmail.com>
2025-08-06 22:31:12 -0500

    Get rid of logger in init file

--- src/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-log = logging.getLogger(__name__)
-logging.basicConfig(
-    #level=logging.DEBUG, 
-    format='[%(asctime)s] [%(levelname)s]: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
